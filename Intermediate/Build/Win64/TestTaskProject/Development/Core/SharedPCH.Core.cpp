09ca852697e227a96c8930765db182cf801256d5
Motya <savmat2000@gmail.com>
2022-01-14 20:46:30 +0300

    Fixes

---
+++ Intermediate/Build/Win64/TestTaskProject/Development/Core/SharedPCH.Core.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.28.29914
+#include "D:/TestTask/TestTaskProject/Intermediate/Build/Win64/TestTaskProject/Development/Core/SharedPCH.Core.h"
---
+++ Intermediate/Source/TestTaskProject.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class TestTaskProject : ModuleRules
+{
+	public TestTaskProject(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
